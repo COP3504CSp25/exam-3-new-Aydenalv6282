0b3653abc14ca506b90924261189482aae7a20a0
Gizmodian <67181460+Aydenalv6282@users.noreply.github.com>
2025-05-01 07:33:27 +0000

    ReverseList attempt 2

--- ReverseList.hpp
+++ ReverseList.hpp
@@ -23,19 +23,10 @@ public:
             revlist.addHead(this->head->data);
             return revlist;
         }
-
-        int size = 0;
         Node* cur_ptr = this->head;
         while(cur_ptr != nullptr){
-            size += 1;
-            cur_ptr = cur_ptr->next;
-        }
-        for(int i=0; i<size; i++){
-            Node* cur_ptr = this->head;
-            for(int j=i+1; j<size; j++){
-                cur_ptr = cur_ptr->next;
-            }
             revlist.addHead(cur_ptr->data);
+            cur_ptr = cur_ptr->next;
         }
         return revlist;
     }
