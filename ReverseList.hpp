856c342d75c668749d3e9d450c3bb4296a7f9a0a
Gizmodian <67181460+Aydenalv6282@users.noreply.github.com>
2025-05-01 07:29:12 +0000

    ReverseList attempt 1

--- ReverseList.hpp
+++ ReverseList.hpp
@@ -15,7 +15,29 @@ public:
     }
     
     LinkedList reverseList() const {
-        // implement your function here!
+        LinkedList revlist;
+        if(this->head==nullptr){
+            return revlist;
+        }
+        if(this->head->next == nullptr){
+            revlist.addHead(this->head->data);
+            return revlist;
+        }
+
+        int size = 0;
+        Node* cur_ptr = this->head;
+        while(cur_ptr != nullptr){
+            size += 1;
+            cur_ptr = cur_ptr->next;
+        }
+        for(int i=0; i<size; i++){
+            Node* cur_ptr = this->head;
+            for(int j=i+1; j<size; j++){
+                cur_ptr = cur_ptr->next;
+            }
+            revlist.addHead(cur_ptr->data);
+        }
+        return revlist;
     }
 
     Node* head = nullptr;
