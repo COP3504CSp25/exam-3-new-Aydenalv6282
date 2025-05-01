cd44c3a005d523ac93d235bf06096da533fdf6d0
Gizmodian <67181460+Aydenalv6282@users.noreply.github.com>
2025-05-01 07:46:15 +0000

    MaxMap Part 1 Attempt 1

--- MaxMap.hpp
+++ MaxMap.hpp
@@ -6,10 +6,22 @@
 #include <vector>
 
 float consultMax(const std::string& search_term, const std::map<std::string, std::vector<float>>& data) {
-    //implement your function here
+    auto it = data.find(search_term);
+    if(it!=data.end()){
+        std::vector<float> vec = it->second;
+        int max = vec.at(0);
+        for(size_t i=0; i<vec.size(); i++){
+            if(vec.at(i) > max){
+                max = vec.at(i);
+            }
+        }
+        return max;
+    }
+    return -1;
 }
 
 std::map<std::string, float> returnMaxMap(const std::map<std::string, std::vector<float>>& in_map) {
     //implement your function here
-    
+    std::map<std::string, float> stuff;
+    return stuff;
 }
